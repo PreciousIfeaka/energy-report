4ea86a78bb74844f9b50c2546251f0386d5278a9
PreciousIfeaka <preciousifeaka@gmail.com>
2026-02-24 17:39:54 +0100

    chore: updated base url

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
     setReportData(null);
 
     try {
-      const response = await fetch(`https://fibre.preciousifeaka.site/api/v1/data/${formData.dataId}/energy-analytics-reports`, {
+      const response = await fetch(`https://fiber.preciousifeaka.site/api/v1/data/${formData.dataId}/energy-analytics-reports`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
