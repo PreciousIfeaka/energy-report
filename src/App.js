34025a03b2bb658cb5f81dc819dd97744525e440
PreciousIfeaka <preciousifeaka@gmail.com>
2026-03-05 05:02:45 +0100

    chore: push

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
     setReportData(null);
 
     try {
-      const response = await fetch(`http://localhost:5001/api/v1/data/${formData.dataId}/energy-analytics-reports`, {
+      const response = await fetch(`https://fiber.preciousifeaka.site/api/v1/data/${formData.dataId}/energy-analytics-reports`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
